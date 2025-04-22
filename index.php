51c21f53ad13b6436c1df5007db8e98ecca2ccd1
Milaim Hajni (Zeka) <amararifi1234@gmail.com>
2025-04-22 19:06:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+$num = -4;
+
+if($num < 0){
+    echo "$num is less then zero";
+}
+
+$age = 18;
+
+echo "<br>";
+
+if (($age > 12) && ($age < 20)) {
+    echo "u iz teenager";
+}
+
+echo "<br>";
+
+if ($age < 18) {
+    echo "u iz under 18";
+}else{
+    echo "u iz adult men";
+}
+echo "<br>";
+$num = 0;
+
+if ($num > 0){
+ echo "Value of \$numb is negativ numbir";  
+}else if ($num == 0){
+    echo "The value of \$num is 0";
+}else{
+    echo "Value of \$numb is pozitiv numbir";    
+}
+
+
+$a = 50;
+$b = 10;
+echo "<br>";
+if ($a == $b){
+    echo "yes";
+}else{
+    echo "no";
+}
+echo "<br>";
+if ($a == $b){
+    echo "1";
+}else if ($a > $b){
+    echo "2";
+}else{
+    echo "3";
+}
